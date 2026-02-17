d4b01c82163feeb2bbd9afe892fcf1bab8e8dda0
RPintre <RPintre@users.noreply.github.com>
2026-02-17 12:46:31 +0100

    Delete Jour-2/client directory

--- Jour-2/client/src/data/myBooks.ts
+++
@@ -1,7 +0,0 @@
-const myBooks = [
-{ id: 1, title: "Le Petit Prince", author: "Saint-Exupéry" },
-{ id: 2, title: "1984", author: "George Orwell" },
-{ id: 3, title: "L'Étranger", author: "Albert Camus" }
-];
-
-export default myBooks;
